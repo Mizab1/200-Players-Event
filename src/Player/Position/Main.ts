6d1792f9606b08ffd592ef4648591c966758fb49
Mizab <mizabmansari@gmail.com>
2024-10-30 20:18:56 +0530

    Added comments

--- src/Player/Position/Main.ts
+++ src/Player/Position/Main.ts
@@ -1,8 +1,9 @@
 import { Data, MCFunction, Objective, raw } from "sandstone";
 
 // Scores
-const playerPosX = Objective.create("player_pos_x", "dummy")("@s");
+export const playerPosX = Objective.create("player_pos_x", "dummy")("@s");
 
+//! Called in 'Tick' function
 export const updatePlayerPos = MCFunction("player/position/update_player_pos", () => {
   raw(`# Get the player pos from their NBT data`);
   const playerDataPosX = Data("entity", "@s").select("Pos[0]");
